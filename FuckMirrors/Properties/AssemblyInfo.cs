5bb1ba095939286510d7eb884a88500ccf28708c
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-08-01 21:07:35 -0500

    refactor

--- FuckMirrors/Properties/AssemblyInfo.cs
+++ FuckMirrors/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameGoesHere")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("FuckMirrors")]
+[assembly: AssemblyDescription("Dab on 'em.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModNameGoesHere")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("Hiatus Labs")]
+[assembly: AssemblyProduct("FuckMirrors")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
